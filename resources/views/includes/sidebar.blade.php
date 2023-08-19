2cc505d7913cb30978a1ca0394ebbfbc5b9422fd
Dimuthu <dimuthu@saberion.lk>
2023-08-20 02:40:37 +0530

    user management

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -79,13 +79,11 @@
                     <a class="sidebar-link sidebar-title link-nav" href="{{route('appointment.index')}}">
                         <i data-feather="book"> </i><span>Appointments</span>
                     </a>
-                </li>
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="javascript:void(0)"><i data-feather="airplay"></i><span class="lan-6">Widgets</span></a>
                     <ul class="sidebar-submenu">
-                      <li><a href="general-widget.html">General</a></li>
-                      <li><a href="chart-widget.html">Chart</a></li>
+                        <li><a href="{{route('user.index', ['role' => 'admin'])}}">Admins</a></li>
+                        <li><a href="{{route('user.index', ['role' => 'consultant'])}}">Consultants</a></li>
                     </ul>
-                  </li>
+                </li>
 
             </ul>
             <br>
