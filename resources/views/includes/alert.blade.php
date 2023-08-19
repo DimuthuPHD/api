a468aed22abc0eaf2f94abe37fb15ac21ae55660
Dimuthu <dimuthu@saberion.lk>
2023-08-20 02:04:19 +0530

    user management changes

--- resources/views/includes/alert.blade.php
+++ resources/views/includes/alert.blade.php
@@ -42,6 +42,38 @@
                 </div>
                 @endif
             </div>
+            @endif
+
+            @if(session('warning'))
+            <div class="alert alert-warning inverse alert-dismissible fade show" role="alert">
+                <i class="icon-bell"></i>
+                <p>{!! session('warning') !!}</p>
+                <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close">
+                    <span class="bg-warning" aria-hidden="true">dismiss</span>
+                </button>
+            </div>
+            @endif
+
+            @if(session('info'))
+            <div class="alert alert-info inverse alert-dismissible fade show" role="alert">
+                <i class="icon-help-alt"></i>
+                <p>{!! session('info') !!}</p>
+                <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close">
+                    <span class="bg-info" aria-hidden="true">dismiss</span>
+                </button>
+            </div>
+            @endif
+
+            @if(session('success'))
+            <div class="alert alert-success inverse alert-dismissible fade show" role="alert">
+                <i class="icon-thumb-up alert-center"></i>
+                <p><b> Well done! </b>{!! session('success') !!}</p>
+                <button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close">
+                    <span class="bg-success" aria-hidden="true">dismiss</span>
+                </button>
+            </div>
+            @endif
         </div>
     </div>
-    @endif
+</div>
+@endif
