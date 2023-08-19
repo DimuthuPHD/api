1f0c4a8d17b9429e0011b4458f73ffd9863787a4
Dimuthu <dimuthu@saberion.lk>
2023-08-19 11:41:41 +0530

    Job seeker complete

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -31,6 +31,11 @@
                         <h4 class="lan-1">General </h4>
                     </div>
                 </li>
+                <li class="sidebar-list">
+                    <a class="sidebar-link sidebar-title link-nav" href="{{route('job-seeker.index')}}">
+                        <i data-feather="user"> </i><span>Job Seekers</span>
+                    </a>
+                </li>
 
                 @if (auth()->user()->isAdmin())
                 <li class="sidebar-list">
