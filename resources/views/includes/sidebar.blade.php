07adc83f07375d609499e91e128580010f2421f5
Dimuthu <dimuthu@saberion.lk>
2023-09-03 23:20:24 +0530

    Fix conflicts

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -31,11 +31,6 @@
                         <h4 class="lan-1">General </h4>
                     </div>
                 </li>
-                <li class="sidebar-list">
-                    <a class="sidebar-link sidebar-title link-nav" href="{{route('job-seeker.index')}}">
-                        <i data-feather="user"> </i><span>Job Seekers</span>
-                    </a>
-                </li>
 
                 <li class="sidebar-list">
                     <a class="sidebar-link sidebar-title link-nav" href="{{route('user.index')}}">
@@ -60,12 +55,6 @@
                         <i data-feather="book"> </i><span>Appointments</span>
                     </a>
                 </li>
-                <li class="sidebar-list"><a class="sidebar-link sidebar-title" href="javascript:void(0)"><i data-feather="airplay"></i><span class="lan-6">Widgets</span></a>
-                    <ul class="sidebar-submenu">
-                      <li><a href="general-widget.html">General</a></li>
-                      <li><a href="chart-widget.html">Chart</a></li>
-                    </ul>
-                  </li>
 
             </ul>
             <br>
