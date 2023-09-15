156363089bfa93f9d0c5de608293b0f70ddd7431
Dimuthu <dimuthu@saberion.lk>
2023-09-16 00:20:58 +0530

    data seeder

--- database/seeders/ConsultantSeeder.php
+++ database/seeders/ConsultantSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Consultant;
 use App\Models\Country;
+use App\Models\JobType;
 use Illuminate\Database\Seeder;
 
 class ConsultantSeeder extends Seeder
@@ -13,11 +14,13 @@ class ConsultantSeeder extends Seeder
      */
     public function run(): void
     {
-        // if (Consultant::get()->count() < 100) {
+        if (Consultant::get()->count() < 100) {
             Consultant::factory(100)->create()->each(function ($consultant) {
                 $countryIds = Country::inRandomOrder()->limit(rand(1, 5))->pluck('id')->toArray();
+                $jobTypeIds = JobType::inRandomOrder()->limit(rand(1, 5))->pluck('id')->toArray();
                 $consultant->countries()->attach($countryIds);
+                $consultant->jobTypes()->attach($jobTypeIds);
             });
-        // }
+        }
     }
 }
