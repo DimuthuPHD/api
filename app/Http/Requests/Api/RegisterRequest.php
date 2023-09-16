9684c9ec07ce0c10554ff29e99a14f0c3a632ee0
Dimuthu <dimuthu@saberion.lk>
2023-09-16 14:57:23 +0530

    register

--- app/Http/Requests/Api/RegisterRequest.php
+++ app/Http/Requests/Api/RegisterRequest.php
@@ -44,7 +44,7 @@ class RegisterRequest extends FormRequest
         throw new HttpResponseException(response()->json([
             'success' => false,
             'message' => 'Validation errors',
-            'data' => $validator->errors(),
+            'errors' => $validator->errors(),
         ]));
     }
 }
