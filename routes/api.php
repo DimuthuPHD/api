a0bddc700b06ea9ef618526f570451cb5b422da9
Dimuthu <dimuthu@saberion.lk>
2023-09-16 11:42:00 +0530

    run cicd

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::get('consultants', [ConsultantController::class, 'index']);
