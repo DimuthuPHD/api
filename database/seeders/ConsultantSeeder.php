6da63119629a810ceb26a638ce53eabeb729d9d5
Dimuthu <dimuthu@saberion.lk>
2023-09-16 14:37:57 +0530

    consultant generate

--- database/seeders/ConsultantSeeder.php
+++ database/seeders/ConsultantSeeder.php
@@ -14,8 +14,9 @@ class ConsultantSeeder extends Seeder
      */
     public function run(): void
     {
-        if (Consultant::get()->count() < 100) {
-            Consultant::factory(100)->create()->each(function ($consultant) {
+        if (Consultant::get()->count() < 250) {
+            $countToCreate = 250 - Consultant::count();
+            Consultant::factory($countToCreate)->create()->each(function ($consultant) {
                 $countryIds = Country::inRandomOrder()->limit(rand(1, 5))->pluck('id')->toArray();
                 $jobTypeIds = JobType::inRandomOrder()->limit(rand(1, 5))->pluck('id')->toArray();
                 $consultant->countries()->attach($countryIds);
