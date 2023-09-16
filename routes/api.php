04b4f8e2abca36b2eafc441b784e5fbd5b14eb2a
Dimuthu <dimuthu@saberion.lk>
2023-09-16 11:45:42 +0530

    check pipeline

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 Route::get('consultants', [ConsultantController::class, 'index']);
